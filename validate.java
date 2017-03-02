e7b81ea92583d53a06a1e1fb938abbcd8bc95446
drbrandon <brandon_lange637@hotmail.com>
2017-03-02 07:23:15 -0400

    Add files via upload
    Added 2 additional error checks

--- validate.java
+++ validate.java
@@ -1,8 +1,20 @@
+import java.util.*;
+
 public class validate {
+			
 	public static int validate_email(String email){
+		ArrayList<String> d = new ArrayList<String>();
+		d.add("hotmail");
+		d.add("gmail");
+		d.add("yahoo");
+		d.add("live");
+		
 		int errors=0;
 		if(!email.contains("@")) errors++;
 		if(!email.contains(".")) errors++;
+		
+		
+		
 		return errors;
 	}
 }
